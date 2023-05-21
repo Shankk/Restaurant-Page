2da0aa38b9cc699ac82e31005c276016db4d6fa1
Shankk <brianviveiros@hotmail.com>
2023-05-21 00:13:07 -0400

    added home page layout

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+import './styles.css';
+import createHome from './home';
+
+
+function createContent() {
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(createContent());
+ 
